53cfbe98deb6b43025743e05e43909231400c174
Letícia Policarpo <121059013+leticiaplcp@users.noreply.github.com>
2025-11-25 12:39:03 -0300

    2º Commit

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+let cardContainer = document.querySelector("main");
+let campoBusca = document.querySelector("header input");
 let dados = [];
 
-async function iniciarBusca() {
+async function carregarDadosIniciais() {
     let resposta = await fetch ("data.json");
     dados = await resposta.json();
-    console.log(dados);
-
+    renderizarCards(dados);
 }
+
+function iniciarBusca() {
+    let termoBusca = campoBusca.value.toLowerCase();
+    let dadosFiltrados = dados.filter(dado => {
+        return dado.nome.toLowerCase().includes(termoBusca) || 
+               dado.descricao.toLowerCase().includes(termoBusca);
+    });
+    renderizarCards(dadosFiltrados);
+}
+
+function renderizarCards(dados) {
+    cardContainer.innerHTML = ""; // Limpa os cards existentes antes de renderizar os novos
+    for (let dado of dados) {
+        let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+        <h2>${dado.nome}</h2>
+        <p>${dado.ano}</p>
+        <p>${dado.descricao}</p>
+        <a href="#">Saiba mais</a>
+        `;
+        cardContainer.appendChild(article);
+    
+    }
+}
+
+carregarDadosIniciais();
